d8ff1229eb54d780271d7077620231f4dd67a3e7
linxiuke <linxiuke@outlook.com>
2017-11-12 12:46:00 +0800

    实现Bean容器

---
+++ src/main/java/pers/lxk/smart4j/framework/util/ReflectionUtil.java
@@ -0,0 +1,70 @@
+package pers.lxk.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @Description: 反射助手类
+ *               使用ClassHelper类获取所加载的类，但是无法通过类实例化对象
+ *               该类用来封装反射相关的API
+ * @Author: linxiuke
+ * @Date: Create in 2017/11/12
+ */
+public final class ReflectionUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     * @param cls
+     * @return
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     * @param object
+     * @param method
+     * @param args
+     * @return
+     */
+    public static Object invokeMethod(Object object, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(object, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量的值
+     * @param object
+     * @param field
+     * @param value
+     */
+    public static void setField(Object object, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (Exception e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
